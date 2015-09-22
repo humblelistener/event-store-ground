76404fe8d4690a0512faf469ce3766d5e33dccf4
davids <davids@pageuppeople.com>
2015-09-22 17:12:29 +1000

    adding code to read all events, and subscribe from a certain point in a stream (incomplete)

--- gesRead.js
+++ gesRead.js
@@ -5,6 +5,7 @@ var connection = gesClient({ host: '192.168.99.100' }),
 
 connection.on('connect', function() {
 
+	// Read a set of events
     // connection.readStreamEventsForward(stream, { start: 0, count: 1 }, function(err, readResult) {
     //     if (err) {
 	// 		return console.log('Ooops!', err); // connection error or stream does not exist
@@ -15,9 +16,23 @@ connection.on('connect', function() {
     //     console.log(readResult.Events[0].OriginalEvent.Data);
     // });
 
-	var subscription = connection.subscribeToStream(stream);
-
-	subscription.on('event', function(event){
-		console.log(event);
+	connection.readAllEventsForward({ maxCount: 10, position: { commitPosition: 0, preparePosition: 0 } }, function(err, readResult) {
+		if (err) {
+			return console.log('Boy oh boy wowee!', err);
+		}
+		console.log(readResult.Events[0].);
 	});
+
+
+	// Method to subscribe to stream for new events
+	// var subscription = connection.subscribeToStream(stream);
+	//
+	// subscription.on('event', function(event){
+	// 	console.log(event);
+	// });
+
+	// var subscription = connection.subscribeToStreamFrom(stream);
+	// subscription.on('event', function(event) {
+	// 	console.log(event);
+	// });
 });
